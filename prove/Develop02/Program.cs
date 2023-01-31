e3b1c7986eaf9aa98d9d8f55a1dccd45a344fc6a
dbrockbn <dnebooks@gmail.com>
2023-01-30 22:49:08 -0700

    changes to fix bugs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("What would you like to do? ");
+        string answer = Console.ReadLine();
+
+
+    
     }
+
+
+
+    
+DateTime theCurrentTime = DateTime.Now;
+string dateText = theCurrentTime.ToShortDateString();
+
 }
