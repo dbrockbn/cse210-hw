c52206809ace73142387d20f9c3666259fc1bd53
dbrockbn <dnebooks@gmail.com>
2023-02-02 17:52:00 -0700

    it didn't previously commit I guess

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        DisplayMenu();
 
+        DisplayJournal();
+
+        PromptGemerator();
+
+        WriteJournal();
+
+        SaveJournal();
+
+        QuitJournal();
+    }
+    private void WriteJournal()
+    {
+
+    }
+        
+    private void DisplayJournal()
+    {
+
+    }
+        
+    private void PromptGemerator()
+    {
+
+    }
+
+    private void DisplayMenu();
+    {
         Console.WriteLine("Please select one of the following choices:");
         Console.WriteLine("1. Write");
         Console.WriteLine("2. Display");
@@ -13,9 +41,30 @@ class Program
         Console.WriteLine("5. Quit");
         Console.Write("What would you like to do? ");
         string answer = Console.ReadLine();
+        
+    } 
+    private void SaveJournal();
+    {
 
+    }
+    private void ExitJournal();
+    {
 
+    }
     
+    while answer = 1 
+    Write write1 = new Write ();
+
+
+    if answer = 2
+
+    if answer = 3
+
+
+    if answer = 4
+
+    if answer = 5
+
     }
 
 
